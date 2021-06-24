3d7b318f25a9852e993da47d4b096fcf1fee5784
barretot <barretoruan77@gmail.com>
2021-06-24 01:29:40 -0300

    Dia 3

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 import 'reflect-metadata';
@@ -8,5 +9,20 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 export { app };
