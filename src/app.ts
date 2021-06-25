15cb4c82813cf68931b2080ee3496efb431f1636
barretot <barretoruan77@gmail.com>
2021-06-25 12:54:35 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import { routes } from './routes';
+import cors from 'cors';
 
 import 'reflect-metadata';
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
